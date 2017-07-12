1f398da477e3e8e408dd6f907b68f64c059878c0
Yuri Alexs <Yuri Alexs>
2017-07-11 21:37:52 -0300

    init

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject player;
+	public float forca;
+	private Rigidbody2D myBody;
+	public bool grounded = false;
+
+
+	 
+
+	public int tempScore;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody2D> ();
+
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("up") || Input.GetKeyDown ("w")) { //GetKey executa enquanto estiver apertando o botão
+
+
+
+			if (grounded) {
+				myBody.velocity = new Vector2 (0f, forca);
+			}
+			Debug.Log("apertou cima");
+		}
+
+		var absY = Mathf.Abs (myBody.velocity.y);
+
+		if (absY < .2f) {
+			grounded = true;
+		} else {
+			grounded = false;
+		}
+
+
+
+	}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
